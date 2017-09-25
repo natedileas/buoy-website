8d54e6a4e7ace2885510ce1895b66109670869b7
Nathan Dileas <ndileas@gmail.com>
2017-09-25 13:48:32 -0400

    More accurate script.

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
-start cmd /k celery -A app:celery worker -l info
+start cmd /k "..\senv\Scripts\celery -A app:celery worker -l info"
 
 start cmd /k redis-server
 
 start cmd /k "cd flask_react && npm start"
 
-env\Scripts\python.exe app.py
+..\senv\Scripts\python.exe app.py
